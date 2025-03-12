0dfdd30a5eee9fec35286056e786c60405ec828e
Achmad Naufal Ferdiansyah <naufansyah31@gmail.com>
2025-03-12 20:49:17 +0700

    first commit

---
+++ app/src/main/java/com/example/androidsqlite/DatabaseManager.java
@@ -0,0 +1,117 @@
+package com.example.androidsqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+import java.util.ArrayList;
+import android.database.Cursor;
+import android.util.Log;
+
+public class DatabaseManager {
+    private static final String ROW_ID = "_id";
+    private static final String ROW_NAMA = "nama";
+    private static final String ROW_TELP = "telp";
+    private static final String NAMA_DB = "database1";
+    private static final String NAMA_TABEL = "tblpelanggan";
+    private static final int DB_VERSION = 3;
+    private static final String CREATE_TABLE = "create table IF NOT EXISTS " + NAMA_TABEL + " (" + ROW_ID + " integer PRIMARY KEY AUTOINCREMENT, " + ROW_NAMA + " text," + ROW_TELP + " text)";
+
+    private final Context context;
+    private DatabaseOpenHelper dbHelper;
+    private SQLiteDatabase db;
+
+    public DatabaseManager(Context ctx) {
+        this.context = ctx;
+        dbHelper = new DatabaseOpenHelper(context);
+        setDb(dbHelper.getWritableDatabase());
+    }
+
+    public SQLiteDatabase getDb() {
+        return db;
+    }
+
+    public void setDb(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    private static class DatabaseOpenHelper extends SQLiteOpenHelper {
+        public DatabaseOpenHelper(Context context) {
+            super(context, NAMA_DB, null, DB_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
+            db.execSQL("DROP TABLE IF EXISTS " + NAMA_TABEL);
+            onCreate(db);
+        }
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public void UpdateRecord(int iId, String sName, String stelp) {
+        ContentValues values = new ContentValues();
+        values.put(ROW_ID, iId);
+        values.put(ROW_NAMA, sName);
+        values.put(ROW_TELP, stelp);
+        try {
+            db.update(NAMA_TABEL, values, ROW_ID + "=" + iId, null);
+        } catch (Exception e) {
+            Log.e("DB ERROR", e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public void DeleteRecord(int Iid) {
+        ContentValues values = new ContentValues();
+        values.put(ROW_ID, Iid);
+        try {
+            db.delete(NAMA_TABEL, ROW_ID + "=" + Iid, null);
+        } catch (Exception e) {
+            Log.e("DB ERROR", e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public void addRow(int Iid, String anama, String atelp) {
+        ContentValues values = new ContentValues();
+        values.put(ROW_ID, Iid);
+        values.put(ROW_NAMA, anama);
+        values.put(ROW_TELP, atelp);
+        try {
+            db.insert(NAMA_TABEL, null, values);
+        } catch (Exception e) {
+            Log.e("DB ERROR", e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<ArrayList<Object>> ambilSemuaBaris() {
+        ArrayList<ArrayList<Object>> dataArray = new ArrayList<>();
+        Cursor cur;
+        try {
+            cur = db.query(NAMA_TABEL, new String[]{ROW_ID, ROW_NAMA, ROW_TELP}, null, null, null, null, null);
+            cur.moveToFirst();
+            if (!cur.isAfterLast()) {
+                do {
+                    ArrayList<Object> dataList = new ArrayList<>();
+                    dataList.add(cur.getLong(0));
+                    dataList.add(cur.getString(1));
+                    dataList.add(cur.getString(2));
+                    dataArray.add(dataList);
+                } while (cur.moveToNext());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Database ERROR", e.toString());
+        }
+        return dataArray;
+    }
+}
---
+++ app/src/main/java/com/example/androidsqlite/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.androidsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.*;
+import java.util.ArrayList;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseManager dm;
+    private EditText eNm, eTelp, ekode;
+    private Button bBaru, bSimpan, bUbah, bHapus;
+    TableLayout tabel4data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dm = new DatabaseManager(this);
+        tabel4data = (TableLayout) findViewById(R.id.table_data);
+        ekode = (EditText) findViewById(R.id.edTextKode);
+        eNm = (EditText) findViewById(R.id.edTextNama);
+        eTelp = (EditText) findViewById(R.id.edTextPhone);
+        bSimpan = (Button) findViewById(R.id.btnSimpan);
+        bUbah = (Button) findViewById(R.id.btnUbah);
+        bHapus = (Button) findViewById(R.id.btnHapus);
+        bBaru = (Button) findViewById(R.id.btnBaru);
+
+        bSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpanTable();
+            }
+        });
+        updateTable();
+        bUbah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ubahTable();
+            }
+        });
+        updateTable();
+        bHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hapusTable();
+            }
+        });
+        updateTable();
+        bBaru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kosongkanField();
+            }
+        });
+        updateTable();
+    }
+
+    protected void simpanTable() {
+        try {
+            dm.addRow(Integer.parseInt(ekode.getText().toString()), eNm.getText().toString(), eTelp.getText().toString());
+            Toast.makeText(getBaseContext(), eNm.getText().toString() + ", berhasil disimpan", Toast.LENGTH_SHORT).show();
+            updateTable();
+            kosongkanField();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "gagal simpan, " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void ubahTable() {
+        try {
+            dm.UpdateRecord(Integer.parseInt(ekode.getText().toString()), eNm.getText().toString(), eTelp.getText().toString());
+            Toast.makeText(getBaseContext(), eNm.getText().toString() + ", berhasil diubah", Toast.LENGTH_SHORT).show();
+            updateTable();
+            kosongkanField();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "gagal ubah, " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void hapusTable() {
+        try {
+            dm.DeleteRecord(Integer.parseInt(ekode.getText().toString()));
+            Toast.makeText(getBaseContext(), "Kode " + ekode.getText().toString() + ", Data Berhasil Dihapus", Toast.LENGTH_SHORT).show();
+            updateTable();
+            kosongkanField();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "gagal hapus, " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void kosongkanField() {
+        eNm.setText("");
+        eTelp.setText("");
+        ekode.setText("");
+    }
+
+    protected void updateTable() {
+        while (tabel4data.getChildCount() > 1) {
+            tabel4data.removeViewAt(1);
+        }
+        ArrayList<ArrayList<Object>> data = dm.ambilSemuaBaris();
+        for (int posisi = 0; posisi < data.size(); posisi++) {
+            TableRow tabelBaris = new TableRow(this);
+            ArrayList<Object> baris = data.get(posisi);
+            TextView idTxt = new TextView(this);
+            idTxt.setTextSize(18);
+            idTxt.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+            idTxt.setText(" " + baris.get(0).toString() + ".");
+            tabelBaris.addView(idTxt);
+            TextView namaTxt = new TextView(this);
+            namaTxt.setTextSize(18);
+            namaTxt.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            namaTxt.setText(baris.get(1).toString());
+            tabelBaris.addView(namaTxt);
+            TextView TelpTxt = new TextView(this);
+            TelpTxt.setTextSize(18);
+            TelpTxt.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            TelpTxt.setText(baris.get(2).toString());
+            tabelBaris.addView(TelpTxt);
+            tabel4data.addView(tabelBaris);
+        }
+    }
+}
